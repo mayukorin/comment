f74bc2079dd37ee0f4e72fbeb9b0366dcc9f00b6
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2024-06-22 15:45:09 +0900

    initialize

---
+++ src/main/java/jp/gihyo/projava/commentlist/CommentListApplication.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projava.commentlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/gihyo/projava/commentlist/CommentListApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projava.commentlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
